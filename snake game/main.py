8f0227f83e01c785a111177c69c457448999ed73
Andriy Demyanyuk <ademya@uw.edu>
2026-09-04 21:13:20 -0700

    snake game high score update

--- snake game/main.py
+++ snake game/main.py
@@ -33,12 +33,12 @@ while game_is_on:
         scoreboard.increase_score()
     
     if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
-        game_is_on = False
-        scoreboard.game_over()
-
+        scoreboard.reset()
+        snake.reset()
+       
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
